41beaa00313ad83a482b194ac694b46139eb9933
Ahmad Arif <arifmassih6@gmail.com>
2023-04-17 02:33:50 -0400

    added portfolio section

--- components/Home/Portfolio.tsx
+++ components/Home/Portfolio.tsx
@@ -5,8 +5,56 @@ import { FaMobileAlt } from "react-icons/fa";
 type Props = {};
 
 export default function Portfolio({}: Props) {
+  const portfolioData = [
+    {
+      icon: <AiFillFolderOpen />,
+      title: "Manage your portfolio",
+      content:
+        "Buy and sell popular digital currencies, keep track of them in one place.",
+    },
+    {
+      icon: <AiFillCalendar />,
+      title: "Recurring buys",
+      content:
+        "Invest in cryptocurrency slowly over time by scheduling buys daily, weekly, or monthly.",
+    },
+    {
+      icon: <RiSafe2Fill />,
+      title: "Vault protection",
+      content:
+        "For added security, store your funds in a vault with time delayed withdrawals.",
+    },
+    {
+      icon: <FaMobileAlt />,
+      title: "Mobile apps",
+      content: (
+        <p>
+          Stay on top of the markets with the Coinbase app for{" "}
+          <a
+            className="text-primary font-normal"
+            href="https://play.google.com/store/apps"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Android
+          </a>{" "}
+          or
+          <a
+            className="text-primary font-normal"
+            href="https://play.google.com/store/apps"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {" "}
+            iOS
+          </a>
+          .
+        </p>
+      ),
+    },
+  ];
   return (
-    <div>
+    <div className="flex flex-col items-center gap-6">
       <div className="text-center space-y-8">
         <h1 className="text-4xl lg:text-5xl font-bold">
           Create your cryptocurrent portfolio today
@@ -16,63 +64,45 @@ export default function Portfolio({}: Props) {
           start trading
         </h2>
       </div>
-
-      <div className="flex flex-col max-w-full">
-        <img src="/portfolio-image.png" alt="portfolio-image" />
+      <div className="flex max-w-7xl">
+        <div className="hidden xl:flex items-center mx-4 lg:max-w-7xl ">
+          <ul className="hidden lg:flex flex-col gap-12">
+            {portfolioData.map((portfolioItem) => {
+              return (
+                <li className="flex  sm:flex-row  gap-4 items-center">
+                  <div className="text-primary text-6xl">
+                    {portfolioItem.icon}
+                  </div>
+                  <div className="text-center sm:text-left space-y-4 px-4">
+                    <h3 className="text-2xl font-medium">
+                      {portfolioItem.title}
+                    </h3>
+                    <p className="font-light">{portfolioItem.content} </p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <img
+          src="/portfolio-image.png"
+          className="sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl"
+          alt=""
+        />
       </div>
-      <div className="flex justify-center">
-        <ul className="[&>li]:space-x-2 space-y-10 text-center">
-          <li className="flex flex-col items-center space-y-4">
-            <AiFillFolderOpen className="text-primary w-16 h-16 " />
-            <h3 className="text-2xl font-medium">Manage your portfolio</h3>
-            <p className="font-extralight">
-              Buy and sell popular digital currencies, keep track of them in one
-              place
-            </p>
-          </li>
-          <li className="flex flex-col items-center space-y-4">
-            <AiFillCalendar className="text-primary w-16 h-16 " />
-            <h3 className="text-2xl font-medium">Recurring buys</h3>
-            <p className="font-extralight">
-              Invest in cryptocurrency slowly over time by scheduling buys
-              daily, weekly, monthly.
-            </p>
-          </li>
-          <li className="flex flex-col items-center space-y-4">
-            <RiSafe2Fill className="text-primary w-16 h-16 " />
-            <h3 className="text-2xl font-medium">Vault protection</h3>
-            <p className="font-extralight">
-              For added security, store your funds in a vault with time delayed
-              withdrawals
-            </p>
-          </li>
-          <li className="flex flex-col items-center space-y-4">
-            <FaMobileAlt className="text-primary w-16 h-16 " />
-            <h3 className="text-2xl font-medium">Mobile apps</h3>
-            <p className="font-extralight">
-              Stay on top of the markets with the CryptoBase app for{" "}
-              <a
-                className="text-primary font-normal"
-                href="https://play.google.com/store/apps"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Android
-              </a>{" "}
-              or{" "}
-              <a
-                className="text-primary font-normal"
-                href="https://www.apple.com/ca/app-store/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                {" "}
-                iOS{" "}
-              </a>
-            </p>
-          </li>
-        </ul>
-      </div>
+      <ul className="xl:hidden flex flex-col  gap-12 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl">
+        {portfolioData.map((portfolioItem) => {
+          return (
+            <li className="flex flex-col sm:flex-row  items-center gap-4 sm:items-start ">
+              <div className="text-primary text-6xl">{portfolioItem.icon}</div>
+              <div className="text-center sm:text-left space-y-4 px-4">
+                <h3 className="text-2xl font-medium">{portfolioItem.title}</h3>
+                <p className="font-light">{portfolioItem.content} </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
