5160aa4cea5f8e9895daa2c3f4d7e6024124dff2
Ahmad Arif <arifmassih6@gmail.com>
2023-04-18 17:56:02 -0400

    fixed overflow scrollbar when not necessary

--- components/Home/Navbar/NavbarDropdown.tsx
+++ components/Home/Navbar/NavbarDropdown.tsx
@@ -4,7 +4,7 @@ type Props = { dropdownContent: ReactElement };
 
 export default function NavbarDropdown({ dropdownContent }: Props) {
   return (
-    <div className="absolute top-full left-0 right-0 border-t-[0.5px] bg-base-100 py-[6vh] border-b-[0.5px] overflow-y-scroll max-h-[70vh]">
+    <div className="absolute top-full left-0 right-0 border-t-[0.5px] bg-base-100 py-[6vh] border-b-[0.5px] overflow-y-auto max-h-[70vh]">
       <div className="max-w-7xl mx-auto flex justify-center items-center ">
         {dropdownContent}
       </div>
